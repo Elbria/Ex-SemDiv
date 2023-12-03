7fb3446d1c912477052a0b51f24eaa8324e2b742
Elbria <elmpriakou@gmail.com>
2023-12-03 15:33:23 -0800

    clean divergent code

--- explainers/contrastive_phrasal/main.sh
+++ explainers/contrastive_phrasal/main.sh
@@ -1,18 +1,14 @@
-output_dir=/fs/clip-divergences/xling-SemDiv/Ex-SemDiv/outputs
-input_dir=/fs/clip-divergences/xling-SemDiv/Ex-SemDiv/annotations
-#model=/fs/clip-divergences/xling-SemDiv/trained_bert_new/from_WikiMatrix.en-es.tsv.filtered_sample_50000.moses.seed/contrastive_divergence_ranking/rdpg
+# French -- ChatGPT paraphrase
+output=${output_dir}/fra_Latn.devtest.equivalents.chatgpt.paraphrase_missing_paired_phrases_reward
+input=/fs/clip-divergences/xling-SemDiv/Ex-SemDiv/flores200_dataset/devtest/fra_Latn.devtest.equivalents.chatgpt.paraphrase_inter
+model_name_or_path=/fs/clip-divergences/xling-SemDiv/trained_bert/from_WikiMatrix.en-fr.tsv.filtered_sample_50000.moses.seed/contrastive_divergence_ranking/rdpg
+python main.py --paired_phrases --missing_phrases --model_name_or_path ${model_name_or_path} --reward --input ${input} --output ${output}
 
-
-#output=${output_dir}/fra_Latn.devtest.equivalents.chatgpt.paraphrase_missing_paired_phrases_reward
-#input=/fs/clip-divergences/xling-SemDiv/Ex-SemDiv/flores200_dataset/devtest/fra_Latn.devtest.equivalents.chatgpt.paraphrase_inter
-#model_name_or_path=/fs/clip-divergences/xling-SemDiv/trained_bert/from_WikiMatrix.en-fr.tsv.filtered_sample_50000.moses.seed/contrastive_divergence_ranking/rdpg
-#python main.py --paired_phrases --missing_phrases --model_name_or_path ${model_name_or_path} --reward --input ${input} --output ${output}
-
-
-#output=${output_dir}/spa_Latn.devtest.equivalents.chatgpt.paraphrase_missing_paired_phrases_reward
-#input=/fs/clip-divergences/xling-SemDiv/Ex-SemDiv/flores200_dataset/devtest/spa_Latn.devtest.equivalents.chatgpt.paraphrase_inter
-#model_name_or_path=/fs/clip-divergences/xling-SemDiv/trained_bert_new/from_WikiMatrix.en-es.tsv.filtered_sample_50000.moses.seed/contrastive_divergence_ranking/rdpg
-#python main.py --paired_phrases --missing_phrases --model_name_or_path ${model_name_or_path} --reward --input ${input} --output ${output}
+# French -- ChatGPT paraphrase
+output=${output_dir}/spa_Latn.devtest.equivalents.chatgpt.paraphrase_missing_paired_phrases_reward
+input=/fs/clip-divergences/xling-SemDiv/Ex-SemDiv/flores200_dataset/devtest/spa_Latn.devtest.equivalents.chatgpt.paraphrase_inter
+model_name_or_path=/fs/clip-divergences/xling-SemDiv/trained_bert_new/from_WikiMatrix.en-es.tsv.filtered_sample_50000.moses.seed/contrastive_divergence_ranking/rdpg
+python main.py --paired_phrases --missing_phrases --model_name_or_path ${model_name_or_path} --reward --input ${input} --output ${output}
 
 
 #output=${output_dir}/ell_Grek.devtest.equivalents.chatgpt.paraphrase_missing_paired_phrases_reward
